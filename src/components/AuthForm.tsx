5b760940d07822101da19c2ca9b446ec0335f609
Ritikchauhan1704 <critik1704@gmail.com>
2024-05-02 00:41:47 +0530

    Login signup ui completed

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -84,9 +84,9 @@ const AuthForm = () => {
         </form>
         <div className="mt-6">
           <div className="relative">
-            <div className="absolute inset-0 flex items-center">
+            <div className="flex items-center">
               <div className="w-full border-t border-gray-300"></div>
-              <div className="relative flex justify-center text-sm ">
+              <div className="absolute inset-x-0 flex justify-center text-sm ">
                 <span className="bg-white px-2 text-gray-500 ">
                   or continue with
                 </span>
